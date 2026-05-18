aed7afd9d65734186ee222dfca39f54b2edfcbf6
Zach Matson <nju4@cdc.gov>
2026-05-18 12:22:47 -0400

    Update cdcalign.js
    Fixed a file name error in cdcalign.js.

--- www/cdcalign.js
+++ www/cdcalign.js
@@ -1,3 +1,3 @@
 $( document ).ready(function() {
-  $( ".navbar .container-fluid" ).append( '<img src="cdc96px.png" align="right">' );
+  $( ".navbar .container-fluid" ).append( '<img src="cdc_logo_96px.png" align="right">' );
 });
